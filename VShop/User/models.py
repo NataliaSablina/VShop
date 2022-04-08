0898bbc9c655644f330eafabfa88e3530ba342b8
NataliaSablina <93759359+NataliaSablina@users.noreply.github.com>
2022-04-08 14:11:40 +0300

    User+Admin

---
+++ VShop/User/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, phone_number, email, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            username=username,
+            phone_number=phone_number,
+            email=self.normalize_email(email)
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, phone_number, email, password=None):
+        user = self.create_user(
+            email=email,
+            username=username,
+            phone_number=phone_number,
+            password=password
+        )
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=50, verbose_name='Name', null=True)
+    phone_number = models.CharField(max_length=12, verbose_name='Phone Number', null=True)
+    email = models.EmailField(unique=True, verbose_name='Email', null=True)
+    password = models.CharField(max_length=100, verbose_name='Password', null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'phone_number']
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
